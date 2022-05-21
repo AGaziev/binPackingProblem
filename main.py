c047e3dbc0c1dc63613c3bf0d659bc684bef38c2
Alan Gaziev <agaziev@stud.kantiana.ru>
2022-05-21 14:16:45 +0300

    increment maxBins if best set not found

--- main.py
+++ main.py
@@ -14,24 +14,27 @@ def inNsystem(a, n):  # перевод в N-мерную систему счис
 
 
 itemCount = 6
-items = [2, 1, 1, 3, 2, 3]
+items = [3, 3, 3, 3, 3, 3]
 binSize = 4
 sum = 0
 for w in items:
     sum += w
 print(sum)
 maxBins = math.ceil(sum / binSize)  # оценка количества ящиков сверху
-for i in range(maxBins ** itemCount):
-    badSet = False  # проверка на валидность набора
-    binStored = [0] * maxBins  # вес который хранит каждый ящик
-    setOfPlaces = inNsystem(i, maxBins)  # набор
-    for item, place in enumerate(setOfPlaces):
-        binStored[place] += items[item]  # загружаем в ящик
-        if binStored[place] > binSize:  # если перебор
-            badSet = True
+bestChoice = None
+while bestChoice is None: # пока не найден лучший набор
+    for i in range(maxBins ** itemCount):
+        badSet = False  # проверка на валидность набора
+        binStored = [0] * maxBins  # вес который хранит каждый ящик
+        setOfPlaces = inNsystem(i, maxBins)  # набор
+        for item, place in enumerate(setOfPlaces):
+            binStored[place] += items[item]  # загружаем в ящик
+            if binStored[place] > binSize:  # если перебор
+                badSet = True
+                break
+        print(setOfPlaces, badSet)
+        if not badSet:
+            bestChoice = setOfPlaces
             break
-    print(setOfPlaces, badSet)
-    if not badSet:
-        bestChoice = setOfPlaces
-        break
+    maxBins += 1 # если не найден набор для такого количества ящиков увеличиваем на единицу
 print(bestChoice)
